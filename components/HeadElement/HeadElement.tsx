8e1db160d5d790fb138bb04b2dfba04238b544cf
Tomek <tomekz@vp.pl>
2022-02-22 13:36:51 +0100

    Add a favicon

--- components/HeadElement/HeadElement.tsx
+++ components/HeadElement/HeadElement.tsx
@@ -9,6 +9,10 @@ export default function HeadElement(): JSX.Element {
       {/* <link rel='canonical' href='' /> */}
       <meta name='description' content='Dashboard for a smart home app' />
       <meta name='author' content='Tomasz Ziółkowski' />
+      <link rel='icon' href='/favicon.ico' sizes='any' />
+      <link rel='icon' href='/reshot-icon-home-UKNYQCE6B2.svg' type='image/svg+xml' />
+      <link rel='apple-touch-icon' href='/apple-touch-icon.png' />
+      <link rel='manifest' href='/manifest.webmanifest' />
     </Head>
   );
 }
