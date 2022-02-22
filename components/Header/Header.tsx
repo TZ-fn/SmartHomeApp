4217cbdb903a329f27833bb16e986131d22b5802
Tomek <tomekz@vp.pl>
2022-02-22 15:58:07 +0100

    Add icons to the control panel, improve the styling

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,19 +1,26 @@
 import { ReactElement } from 'react';
 import Image from 'next/image';
 import SmartHomeLogo from '../../public/reshot-icon-home-UKNYQCE6B2.svg';
+import LoginIcon from '../../public/reshot-icon-create-user-RGEUQASVK2.svg';
+import SettingsIcon from '../../public/reshot-icon-settings-MDHEAFCKRV.svg';
 import styles from './Header.module.scss';
 
 export default function Header(): ReactElement {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.logoContainer}>
-        <Image height={66} width={66} src={SmartHomeLogo.src} />
+        <Image layout='fixed' height={70} width={70} src={SmartHomeLogo.src} />
         <h1>SmartHome</h1>
       </div>
       <div className={styles.controlPanelContainer}>
-        <div>2asdfasdfasdf</div>
-        <div>3asdfasdfasdf</div>
-        <div>4asdfasdfasdf</div>
+        <div>
+          <Image height={33} width={33} src={LoginIcon.src} />
+          <p>Login/Account</p>
+        </div>
+        <div>
+          <Image height={33} width={33} src={SettingsIcon.src} />
+          <p>Settings</p>
+        </div>
       </div>
     </header>
   );
