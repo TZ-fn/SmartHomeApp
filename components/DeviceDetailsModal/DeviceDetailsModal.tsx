63b30f2f60e4c5869041ace711eb731d768b293e
Tomek <tomekz@vp.pl>
2022-02-26 14:17:09 +0100

    Add new elements to the device modal window

--- components/DeviceDetailsModal/DeviceDetailsModal.tsx
+++ components/DeviceDetailsModal/DeviceDetailsModal.tsx
@@ -1,6 +1,7 @@
 import { ReactElement, useEffect, useState } from 'react';
 import interact from 'interactjs';
 import styles from './DeviceDetailsModal.module.scss';
+import { DragEvent } from '@interactjs/types';
 
 export default function DeviceDetailsModal(): ReactElement {
   const [position, setPosition] = useState({ x: 150, y: 100 });
@@ -10,7 +11,7 @@ export default function DeviceDetailsModal(): ReactElement {
     if (draggableModal !== undefined) {
       draggableModal.draggable({
         listeners: {
-          move(event) {
+          move(event: DragEvent) {
             setPosition({ ...position, x: (position.x += event.dx) });
             setPosition({ ...position, y: (position.y += event.dy) });
             event.target.style.transform = `translate(${position.x}px, ${position.y}px)`;
@@ -22,7 +23,17 @@ export default function DeviceDetailsModal(): ReactElement {
 
   return (
     <div className={`${styles.modalContainer} draggable`}>
-      <div className={styles.modal}></div>
+      <div className={styles.modal}>
+        <p className={styles.modalDeviceName}>Smart Temperature Sensor</p>
+        <p className={styles.modalDeviceID}>ID: E78C0467EF</p>
+        <p className={styles.modalConnectionState}>
+          Connection:
+          <span className={`${styles.modalConnectionStateText} ${styles.textConnected}`}>
+            connected
+          </span>
+          .<span className={`${styles.modalConnectionStateIcon} ${styles.connected}`}></span>
+        </p>
+      </div>
     </div>
   );
 }
