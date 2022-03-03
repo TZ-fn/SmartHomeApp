fc135beb744036e836278e0810ea27a1e6c03ffa
Tomek <tomekz@vp.pl>
2022-03-03 14:44:22 +0100

    Change nextjs configuration to make webSocketServer work

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-  reactStrictMode: true,
+  eactStrictMode: true,
   i18n: {
     locales: ['en'],
     defaultLocale: 'en',
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
