0dfea15e7aaa52a8fd46421926a7995ba61bdcaa
Tomek <tomekz@vp.pl>
2022-03-04 15:21:50 +0100

    Add a custom useFetch hook to handle fetching the data from the API

---
+++ hooks/useFetch.tsx
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react';
+
+export const useFetch = (APIurl: string) => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(APIurl);
+        const data = await response.json();
+        if (response.status === 404) {
+          throw new Error(data?.message);
+        }
+        setData(data);
+      } catch (error) {
+        if (error instanceof Error) {
+          console.log(error);
+        }
+      }
+    };
+    fetchData();
+  }, [APIurl]);
+
+  return [data];
+};
