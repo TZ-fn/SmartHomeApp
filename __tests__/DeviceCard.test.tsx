f88be4a2166fed64cd10494f8972546bd17dabc9
Tomek <tomekz@vp.pl>
2022-03-10 15:17:21 +0100

    Add tests to the DeviceCard component, work in progress

---
+++ __tests__/DeviceCard.test.tsx
@@ -0,0 +1,46 @@
+import { render, screen } from '@testing-library/react';
+import DeviceCard from '../components/DevicesList/DeviceCard/DeviceCard';
+import { ConnectionStateType, SmartDeviceType } from '../mockedAPIdata/devices';
+
+const deviceTypes = {
+  bulb: 'bulb',
+  outlet: 'outlet',
+  temperatureSensor: 'temperatureSensor',
+};
+
+const deviceNames = {
+  smartBulb: 'Smart Bulb',
+  smartOutlet: 'Smart Outlet',
+  smartTemperatureSensor: 'Smart Temperature Sensor',
+};
+
+const connectionStates = {
+  connected: 'connected',
+  disconnected: 'disconnected',
+  poorConnection: 'poorConnection',
+};
+
+describe('Tests for the DeviceCard component', () => {
+  it('renders correctly', () => {
+    render(
+      <DeviceCard
+        type={deviceTypes.bulb as SmartDeviceType}
+        id={'1'}
+        name={deviceNames.smartBulb}
+        key={'1'}
+        connectionState={connectionStates.connected as ConnectionStateType}
+        onClick={(e) => e}
+      />,
+    );
+    const deviceName = RegExp(`${deviceNames.smartBulb}`);
+    expect(screen.getByText(deviceName)).toBeInTheDocument();
+  });
+
+  // it('renders the user control icons correctly', () => {
+  //   render(<DeviceCard />);
+  //   const loginButton = screen.getByText(/Login\/Account/);
+  //   const settingsButton = screen.getByText(/Settings/);
+  //   expect(loginButton).toBeInTheDocument();
+  //   expect(settingsButton).toBeInTheDocument();
+  // });
+});
