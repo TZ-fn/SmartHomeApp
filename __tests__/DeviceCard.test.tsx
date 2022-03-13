3545c18f31ec9ea8f548b17d52e83f02485b2e3d
Tomek <tomekz@vp.pl>
2022-03-13 15:09:58 +0100

    Move the mocked tests data to a separate file

--- __tests__/DeviceCard.test.tsx
+++ __tests__/DeviceCard.test.tsx
@@ -1,24 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import DeviceCard from '../components/DevicesList/DeviceCard/DeviceCard';
+import { deviceTypes, deviceNames, connectionStates } from './mockedTestsData';
 import { ConnectionStateType, SmartDeviceType } from '../mockedAPIdata/devices';
-
-const deviceTypes = {
-  bulb: 'bulb',
-  outlet: 'outlet',
-  temperatureSensor: 'temperatureSensor',
-};
-
-const deviceNames = {
-  smartBulb: 'Smart Bulb',
-  smartOutlet: 'Smart Outlet',
-  smartTemperatureSensor: 'Smart Temperature Sensor',
-};
-
-const connectionStates = {
-  connected: 'connected',
-  disconnected: 'disconnected',
-  poorConnection: 'poorConnection',
-};
 
 describe('Tests for the DeviceCard component', () => {
   it('renders correctly', () => {
@@ -32,32 +15,7 @@ describe('Tests for the DeviceCard component', () => {
         onClick={(e) => e}
       />,
     );
-    const deviceName = RegExp(deviceNames.smartBulb);
+    const deviceName = new RegExp(deviceNames.smartBulb);
     expect(screen.getByText(deviceName)).toBeInTheDocument();
   });
-
-  it('is styled properly', () => {
-    render(
-      <DeviceCard
-        type={deviceTypes.bulb as SmartDeviceType}
-        id={'1'}
-        name={deviceNames.smartBulb}
-        key={'1'}
-        connectionState={connectionStates.disconnected as ConnectionStateType}
-        onClick={(e) => e}
-      />,
-    );
-    const connectionInfo = screen.getByText(/Disconnected/);
-    // console.log(window.getComputedStyle(connectionInfo));
-    console.log(screen);
-    expect(connectionInfo).toHaveStyle('visibility: visible;');
-  });
-
-  // it('renders the user control icons correctly', () => {
-  //   render(<DeviceCard />);
-  //   const loginButton = screen.getByText(/Login\/Account/);
-  //   const settingsButton = screen.getByText(/Settings/);
-  //   expect(loginButton).toBeInTheDocument();
-  //   expect(settingsButton).toBeInTheDocument();
-  // });
 });
---
+++ __tests__/mockedTestsData.js
@@ -0,0 +1,17 @@
+export const deviceTypes = {
+  bulb: 'bulb',
+  outlet: 'outlet',
+  temperatureSensor: 'temperatureSensor',
+};
+
+export const deviceNames = {
+  smartBulb: 'Smart Bulb',
+  smartOutlet: 'Smart Outlet',
+  smartTemperatureSensor: 'Smart Temperature Sensor',
+};
+
+export const connectionStates = {
+  connected: 'connected',
+  disconnected: 'disconnected',
+  poorConnection: 'poorConnection',
+};
